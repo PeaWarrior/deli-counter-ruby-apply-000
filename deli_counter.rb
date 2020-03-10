748a49fd225c77d0a05d6d97313c8efb691e16a8
Jackson Chen <jacksonchen171@gmail.com>
2020-03-10 13:10:01 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,4 +1,14 @@
 
-def line(deli)
+def line(katz_deli)
+  if katz_deli.length == 0
+    puts "The line is currently empty"
+  else
+    queue = ""
+    count = 1
+    katz_deli.each do |customer|
+      queue += count + ". " + customer
+    end
+    puts "The line is currently: " +queue
+  end
 
 end

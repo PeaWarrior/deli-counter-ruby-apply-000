43024fb6ea153914d573ebd0bb882e1d405f791c
Jackson Chen <jacksonchen171@gmail.com>
2020-03-10 13:23:03 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -14,5 +14,7 @@ def line(katz_deli)
 end
 
 def take_a_number(katz_deli, newcustomer)
-  puts "Welcome, #{newcustomer}. You are number #{katz_deli.length+1} in line."
+  katz_deli.push(newcustomer)
+  puts "Welcome, #{newcustomer}. You are number #{katz_deli.length} in line."
+
 end
